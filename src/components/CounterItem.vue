f59f6557739b433e078d88bd634244603aba88d7
SebasPuru123 <122682780+SebasPuruguay@users.noreply.github.com>
2023-05-23 08:26:03 -0500

    Initial commit

---
+++ src/components/CounterItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h5>{{ fullName }}</h5>
+    <div>
+      <h4>El numero actual es...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="count--">-</button>
+      <button @click="count++">+</button>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "CounterItem",
+  data() {
+    return {
+      count: 0,
+      fullName: "Sebas Puruguay",
+    };
+  },
+};
+</script>
